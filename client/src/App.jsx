968cc1dc007a3a6af9149ebbf282834f14b3097f
George <georgios.orfanidis4@gmail.com>
2025-07-08 16:46:18 +0300

    feat: Added routing for basic pages.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+// General
+import { Routes, Route } from 'react-router-dom';
+
+// Styles
+import './Index.css'
 import './App.css'
 
-function App() {
- 
+// Pages
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/AboutUs'
+import Login from './pages/Login'
 
-  return (
-    <>
-    
-    </>
-  )
+// States
+
+function App() {
+ return (
+  <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/contact' element={<Contact />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/login' element={<Login />} />
+  </Routes>
+ );
 }
 
 export default App
